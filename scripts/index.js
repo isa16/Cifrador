98b37ba3ccc0ab79539a67ac38cc4a364797d967
Isabela Crivelli <isabela.crivelli@novaandradina.or>
2019-04-04 12:31:46 -0400

    Arrumando o js

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,51 @@
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alfabeto = ["a", "b", "c", "d", "e", "f",
+    "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-const criptografar = () => {
-    let palavra = capturaInput()
+function capElemento(ref) {
+    const element = document.querySelector(ref)
+    return element
+}
+
+const formu = capElemento('form')
+formu.addEventListener('submit', function (event){
+    verificChave();
+    event.preventdefault();    
+}, false)
+
+const verificChave = () => {
+    const key = capElemento('senha');
+    if(key <= 26 || key > 0 ){
+criptografar();
+        
+    }else{
+        alert('Chave inválida!');
+    }
+}
+
+const word = capElemento('palavra');
+
+
+
+const criptografar = (key) => {
+    let chave = parseInt(key);
+    let word = capElemento('palavra').toLowerCase();
     let retorno = ""
-    let num = sorteiaNum()
 
-    for (let i = 0; i < palavra.length; i++) {
-        if (palavra[i] == " ") {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] != " ") {
             retorno += "-"
         } else {
             let letraCifrada = null
             let j = 0
-            while (letraCifrada != palavra[i]) {
+            while (letraCifrada != word[i]) {
                 letraCifrada = alfabeto[j]
                 j++
             }
-            if (j < alfabeto.length + num) {
-                retorno += alfabeto[j + num]
+            if (j < alfabeto.length + key) {
+                retorno += alfabeto[j + key]
             }
-            else if (j > alfabeto.length + num) {
+            else if (j > alfabeto.length + key) {
                 retorno += alfabeto[j]
             }
             else {
@@ -26,28 +53,29 @@ const criptografar = () => {
             }
         }
     }
-    retorno += `${num}`
-    devolveHTML(retorno)
+    retorno += `${retorno}`
+    retornaHTML(retorno)
 }
 
 const descriptografar = () => {
-    let palavra = capturaInput()
+    let chave = parseInt(key);
+    let word = capElemento('palavra').toLowerCase();
     let retorno = ""
-    let num = palavra[palavra.length-1]
-    for (let i = 0; i < palavra.length - 1; i++) {
-        if (palavra[i] == "-") {
+    let num = word[word.length - 1]
+    for (let i = 0; i < word.length - 1; i++) {
+        if (word[i] == "-") {
             retorno += " "
         } else {
             let letraCifrada = null
             let j = 26
-            while (letraCifrada != palavra[i]) {
+            while (letraCifrada != word[i]) {
                 letraCifrada = alfabeto[j]
                 j--
             }
-            if (j < alfabeto.length - num) {
-                retorno += alfabeto[j - num]
+            if (j < alfabeto.length - chave) {
+                retorno += alfabeto[j - chave]
             }
-            else if (j > alfabeto.length - num) {
+            else if (j > alfabeto.length - chave) {
                 retorno += alfabeto[j]
             }
             else {
@@ -55,25 +83,12 @@ const descriptografar = () => {
             }
         }
         console.log(retorno);
-        
+
     }
-    devolveHTML(retorno)
+    retornaHTML(retorno)
 }
 
-const capturaInput = () => {
-    return document.getElementsByTagName('input')[0].value
-}
 
-const devolveHTML = (elemento) => {
+const retorneHTML = (elemento) => {
     document.getElementById('retorno').innerHTML = elemento;
-}
-
-const sorteiaNum = () => {
-    let num = parseInt(Math.random() * 7)
-    if (num == 0) {
-        while (num == 0) {
-            num = parseInt(Math.random() * 7)
-        }
-    }
-    return num
 }
